c8d50724132043365172d266e9e328a25b6aa0f4
Pratheeksh E J <pratheeksh@thinking-code.com>
2019-09-07 13:53:01 +0530

    Chage in APP JS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      val: 'Pratheeksh E J'
+    };
+  }
+
+  transformVal = {
+    val: 'Pratheeksh E J'
+  };
+
+  setValue(value) {
+    this.transformVal.val = value.val;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hey This is { this.state.val }
+          </p>
+          <button onClick={() => this.setState({val: 'P E J'})}>TRANSFORM</button>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
