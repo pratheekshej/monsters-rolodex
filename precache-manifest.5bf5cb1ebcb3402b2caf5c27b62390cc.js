186f4eeef1f97eab082a382c323cb26d5bc7939c
Pratheeksh E J <pratheeksh@thinking-code.com>
2019-09-22 13:57:25 +0530

    Updates

---
+++ precache-manifest.5bf5cb1ebcb3402b2caf5c27b62390cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b62888e77730b1c009dec3ea5497a9b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "14ac84a324beb5c5a731",
+    "url": "/monsters-rolodex/static/css/main.47fbe688.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "14ac84a324beb5c5a731",
+    "url": "/monsters-rolodex/static/js/main.880c28c9.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
